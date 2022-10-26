f6e2476d468c2af128bbd919e72ab33b11eb2a56
Jasmin Juzbasic <jasmin.juzbasic@gmail.com>
2022-10-26 17:11:08 +0200

    add worksheets for all restaurants and get their values, sketch out and organize idea with comments

--- run.py
+++ run.py
@@ -29,3 +29,25 @@ mommes_score = mommes.get_all_values()
 libanesiska_score = libanesiska.get_all_values()
 
 print(nata_score)
+
+# Display list of cities and validate their choice
+
+# Present the user with the list of restaurants and ask them to select one, validate their choice
+
+# Collect collumns of data from the chosen restaurant from the spreadsheet and present individual scores 
+# and total scores. Validate their choice.
+
+# Ask if they wish to proceed, if yes: remove the restaurants they have already selected from the list
+# add another option in the list, view total scores for all restaurants. 
+# add another option in the list, that exits the program.
+# if no: ask for name and what restaurant they want to eat at.
+# add the name and restaurant to a tab in the spreadsheet, validate their answer and exit the program.
+
+# Repeat until user has viewed the scores for all restaurants, 
+# or until they decide on the restaurant and exit the program.
+
+# When user has decided on a restaurant, ask them for name and where they will eat and then exit the program.
+
+# Validate their answer, and save restaurant where they will eat + name in a tab in the spreadsheet.
+
+
