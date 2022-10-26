047a6a6c5539e4a6a9698831bcf98ab5e3c2c8a5
Jasmin Juzbasic <jasmin.juzbasic@gmail.com>
2022-10-26 03:45:20 +0200

    wire up APIs and confirm access to survey-answers in google sheets

--- run.py
+++ run.py
@@ -1,3 +1,31 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import gspread
+from google.oauth2.service_account import Credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('the_fastfood_survey')
+
+nata = SHEET.worksheet('Nata')
+paradis = SHEET.worksheet('Paradis')
+frikkos = SHEET.worksheet('Frikkos')
+babylon = SHEET.worksheet('Babylon')
+yazhou = SHEET.worksheet('Yazhou')
+mommes = SHEET.worksheet('Mommes')
+libanesiska = SHEET.worksheet('Libanesiska')
+
+nata_score = nata.get_all_values()
+paradis_score = paradis.get_all_values()
+frikkos_score = frikkos.get_all_values()
+babylon_score = babylon.get_all_values()
+yazhou_score = yazhou.get_all_values()
+mommes_score = mommes.get_all_values()
+libanesiska_score = libanesiska.get_all_values()
+
+print(nata_score)
