11c46d5988a7015ea2abe99517daafdc83bd3d6f
Jasmin Juzbasic <jasmin.juzbasic@gmail.com>
2022-10-27 13:32:03 +0200

    add function for get restaurant and their scores

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+from email.headerregistry import HeaderRegistry
 import gspread
 from google.oauth2.service_account import Credentials
 
@@ -28,6 +29,8 @@ babylon_score = babylon.get_all_values()
 yazhou_score = yazhou.get_all_values()
 mommes_score = mommes.get_all_values()
 libanesiska_score = libanesiska.get_all_values()
+
+
 
 print("Are you hungry? Let us help you with that.\n")
 
@@ -64,16 +67,13 @@ def get_city():
     city_str = input("Where are you located? Enter a number:\n 1. Helsingborg\n 2. Göteborg\n 3. Malmö\n ")
     while True:
         if city_str == "1":
-            print("Here are the restaurants in Helsingborg:\n")
-            print("1. Nata\n2. Paradis\n3. Frikkos\n4. Babylon\n5. Yazhou\n6. Mommes\n7. Libanesiska\n")
+            print("You have chosen Helsingborg. Here are the restaurants in Helsingborg:\n")
             break
         elif city_str == "2":
-            print("Here are the restaurants in Göteborg:\n")
-            print("1. Babylon\n2. Yazhou\n3. Mommes\n")
+            print("You have chosen Göteborg. Here are the restaurants in Göteborg:\n")
             break
         elif city_str == "3":
-            print("Here are the restaurants in Malmö:\n")
-            print("1. Libanesiska\n")
+            print("You have chosen Malmö. Here are the restaurants in Malmö:\n")
             break
         else:
             print("Try again, please select a number between 1-3\n")
@@ -83,12 +83,66 @@ def get_city():
 
 # print(get_city())
 
+def get_restaurant():
+    """Gets the restaurant from the user"""
+    restaurant_str = input("Select a restaurant from 1-7 to see their score!\n 1. Nata\n 2. Paradis\n 3. Frikkos\n 4. Babylon\n 5. Yazhou\n 6. Mommes\n 7. Libanesiska\n ")
+    while True:
+        if restaurant_str == "1":
+            print("You have chosen Nata.\n")
+            break
+        elif restaurant_str == "2":
+            print("You have chosen Paradis.\n")
+            break
+        elif restaurant_str == "3":
+            print("You have chosen Frikkos.\n")
+            break
+        elif restaurant_str == "4":
+            print("You have chosen Babylon.\n")
+            break
+        elif restaurant_str == "5":
+            print("You have chosen Yazhou.\n")
+            break
+        elif restaurant_str == "6":
+            print("You have chosen Mommes.\n")
+            break
+        elif restaurant_str == "7":
+            print("You have chosen Libanesiska.\n")
+            break
+        else:
+            print("Try again, please select a number between 1-7\n")
+            restaurant_str = input("Which restaurant do you want to see?\n 1. Nata\n 2. Paradis\n 3. Frikkos\n 4. Babylon\n 5. Yazhou\n 6. Mommes\n 7. Libanesiska\n ")
+            continue
+    return restaurant_str
+
+def display_scores(city_str, restaurant_str):
+    """Displays the scores for the selected restaurant"""
+    restaurant_score = (city_str, restaurant_str)
+    print(restaurant_score)
+
+    while True:
+        if  get_city() == "1" and get_restaurant() == "1":
+            print(nata.get_all_values())
+        elif get_city() == "1" and get_restaurant() == "2":
+            print(paradis.get_all_values())
+        elif get_city() == "1" and get_restaurant() == "3":
+            print(frikkos.get_all_values())
+        elif get_city() == "1" and get_restaurant() == "4":
+            print(babylon.get_all_values())
+        elif get_city() == "1" and get_restaurant() == "5":
+            print(yazhou.get_all_values())
+        elif get_city() == "1" and get_restaurant() == "6":
+            print(mommes.get_all_values())
+        elif get_city() == "1" and get_restaurant() == "7":
+            print(libanesiska.get_all_values())
+
 def main():
     """
     Run all program functions
     """
     get_name()
-    get_city()
+    city = get_city()
+    restaurant = get_restaurant()
+    display_scores(city, restaurant)
 
 main()
 
